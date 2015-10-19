c37925d278cdc11916adaf00b70e55e9dc28ac02
Keyfoxth <vipwangyw@hotmail.com>
2015-10-19 04:19:39 +0000

    Finish user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # 登入用户，然后重定向到用户的资料页面
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
