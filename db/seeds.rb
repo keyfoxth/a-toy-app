ccd2b8e42ca3f8e48c205e5b870c471a9d2f6f83
Keyfoxth <vipwangyw@hotmail.com>
2015-10-22 08:35:51 +0000

    Finish user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
-98.times do |n|
+User.create!(name: "Keyfoxth",
+             email: "vipwangyw@hotmail.com",
+             password: "123456",
+             password_confirmation: "123456",
+             admin: true)
+
+99.times do |n|
 name = Faker::Name.name
 email = "example-#{n+1}@railstutorial.org"
 password = "password"
